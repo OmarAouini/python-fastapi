d13451e00b94efe5a98fd322f17fbea5ace35fe1
OmarAouini <aouini.omar93@gmail.com>
2022-05-26 00:40:04 +0200

    field validation in schema dto

--- schemas.py
+++ schemas.py
@@ -1,10 +1,10 @@
 """REQUEST BODY SCHEMAS MODULE"""
 from typing import Union
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Item(BaseModel):
     """sample dto schema"""
     name: str
     description: Union[str, None] = None
-    price: float
+    price: float = Field(gt=0, description="price must be greater than 0")
     tax: Union[float, None] = None
