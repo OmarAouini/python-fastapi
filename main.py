5a11a536f227421d00882eb3241c7d246b107824
OmarAouini <aouini.omar93@gmail.com>
2022-05-26 01:29:59 +0200

    added example endpont with auth

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """MAIN"""
 import logging
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 import uvicorn
+
+from auth_keycloak import User, get_current_active_user
 
 # logger
 logger = logging.getLogger("uvicorn")
@@ -15,6 +17,11 @@ DB_NAME = "employees"
 async def health():
     """healthcheck endpoint"""
     return {"result":"OK"}
+
+# example endpoint with auth user
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
 
 # run
 if __name__ == "__main__":
