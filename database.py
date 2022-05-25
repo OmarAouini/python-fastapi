220898fbee538ae56ca75066c24f7dbcb54c0d26
OmarAouini <aouini.omar93@gmail.com>
2022-05-26 01:21:33 +0200

    change db connectionstring

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
+"""SQLALCHEMY DATABASE MODULE"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'db_url'
+SQLALCHEMY_DATABASE_URL = 'mysql://localhost:3306/employees'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
