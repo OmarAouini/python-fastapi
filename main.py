c83653ff9c2b300a1a5a9332993349c07683f58a
OmarAouini <aouini.omar93@gmail.com>
2022-05-25 23:18:49 +0200

    added dockerfile, print db env

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Response
 import uvicorn
 
-
 app = FastAPI()
 ENV = "dev" #TODO: set dotenv file
+DB_NAME = "employees"
 
 # health check endpoint
 @app.get("/", status_code=200)
-def health():
+async def health():
     """healthcheck endpoint"""
     return Response(status_code=200)
 
@@ -16,5 +16,6 @@ if __name__ == "__main__":
     print("AUTHOR: OMAR AOUINI, 2022")
     print("SAMPLE REST API")
     print(f"ENV: {ENV}")
+    print(f"DB NAME: {DB_NAME}")
     print("============================")
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
