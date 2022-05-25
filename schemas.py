2cdf238cc9da63c76561cf3cdcaba8c9dd3c6b4c
OmarAouini <aouini.omar93@gmail.com>
2022-05-26 00:25:12 +0200

    cleanup docstring

--- schemas.py
+++ schemas.py
@@ -1,8 +1,9 @@
+"""REQUEST BODY SCHEMAS MODULE"""
 from typing import Union
 from pydantic import BaseModel
 
-
 class Item(BaseModel):
+    """sample dto schema"""
     name: str
     description: Union[str, None] = None
     price: float
